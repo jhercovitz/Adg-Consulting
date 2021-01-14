4d537941fce3b2cfeb3b7ff0a4b8a73e452859e7
johann_dev <johann.hercovitz@gmail.com>
2021-01-14 22:55:06 +0100

    9th commit

--- Adg_Consulting.js
+++ Adg_Consulting.js
@@ -13,25 +13,64 @@ anime.timeline({loop: true})
   }).add({
     targets: '.ml2',
     opacity: 0,
-    duration: 10000000,
+    duration: 1000000,
     easing: "easeOutExpo",
     delay: 1000
   });
 
   var textWrapper = document.querySelector('.ml16');
-textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+  textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>")
+
 
 anime.timeline({loop: true})
   .add({
-    targets: '.ml16 .letter',
+    targets: '.ml16 .letter  ',
     translateY: [-100,0],
     easing: "easeOutExpo",
     duration: 1400,
     delay: (el, i) => 30 * i
   }).add({
-    targets: '.ml16',
+    targets: '.ml16 .letter  ',
     opacity: 0,
-    duration: 10000000,
+    duration: 1000000,
+    easing: "easeOutExpo",
+    delay: 1000
+  });
+
+  var textWrapper = document.querySelector('.ml17');
+  textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>")
+
+
+anime.timeline({loop: true})
+  .add({
+    targets: '.ml17 .letter  ',
+    translateY: [-100,0],
+    easing: "easeOutExpo",
+    duration: 1400,
+    delay: (el, i) => 30 * i
+  }).add({
+    targets: '.ml17 .letter  ',
+    opacity: 0,
+    duration: 1000000,
+    easing: "easeOutExpo",
+    delay: 1000
+  });
+
+  var textWrapper = document.querySelector('.ml18');
+  textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>")
+
+
+anime.timeline({loop: true})
+  .add({
+    targets: '.ml18 .letter  ',
+    translateY: [-100,0],
+    easing: "easeOutExpo",
+    duration: 1400,
+    delay: (el, i) => 30 * i
+  }).add({
+    targets: '.ml18 .letter  ',
+    opacity: 0,
+    duration: 1000000,
     easing: "easeOutExpo",
     delay: 1000
   });
